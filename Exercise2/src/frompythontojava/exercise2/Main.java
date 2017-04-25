a9aa1216930e984e10d93e415866c091a1bef87e
Marti Kecskemeti <martikecskemeti@gmail.com>
2017-04-25 12:21:04 +0200

    initial commit

---
+++ Exercise2/src/frompythontojava/exercise2/Main.java
@@ -0,0 +1,8 @@
+package frompythontojava.exercise2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Java SE: Interface and abstract class
+
+Java SE 2nd SI week, Exercise 2
